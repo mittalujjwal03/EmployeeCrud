ea8bc37673cb1d74fd73c300f7674cc9937b77f5
Ujjwal Mittal <ujjwal.ginilytics@gmail.com>
2024-10-04 16:36:01 +0530

    Commit/Ujjwal/UI_Btn_Fix

--- EmployeeCRUD/obj/Debug/net8.0/EmployeeCRUD.AssemblyInfo.cs
+++ EmployeeCRUD/obj/Debug/net8.0/EmployeeCRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeCRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b2b2ce4959c5456d101c843ea908a79af205e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce941d4c6a4ce4ddac6dc19a784bfdccee6d9b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeCRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeCRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
